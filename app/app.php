0315af83df78b631b1e4ffa9744a60dcae650168
Ronando Long <ronando.long@gmail.com>
2017-01-31 16:15:10 -0800

    add functionality for user to submit task, and view task using Twig template files, next is delete

--- app/app.php
+++ app/app.php
@@ -1,4 +1,5 @@
 <?php
+
     date_default_timezone_set('America/Los_Angeles');
     require_once __DIR__."/../vendor/autoload.php";
     require_once __DIR__."/../src/tasks.php";
@@ -10,87 +11,26 @@
 
     $app = new Silex\Application();
 
-    $app->get("/", function() {
-        $output = "";                  // 1. create output variable for browser
-        $all_tasks = Tasks::getAll();  // 2. create variable for array
-                                       // 3. print out homepage HTML
-        $output .= "
-          <!DOCTYPE html>
-          <html>
-            <head>
-              <meta charset='utf-8'>
-              <title>Tasks: To Do List</title>
-              <link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css'>
-            </head>
+    $app->register(new Silex\Provider\TwigServiceProvider(), array(
+        'twig.path' => __DIR__.'/../views'
+    ));
+  // End Red Tape
 
-            <body>
-              <div class='container'>
-                <h1>Tasks: To Do List</h1>
-                <div class='row'>
-                  <div class='col-md-4'>
-        ";
-
-        if (!empty($all_tasks)) {         // 4. IF array of tasks is NOT empty
-            foreach ($all_tasks as $task) {
-                $output .= "
-                    <p>" . $task->getTask() . "</p>
-              ";
-            }
-        }
-        $output .= "
-                    <form action='/tasks' method='post'>
-                      <div class='form-group'>
-                        <label for='task'>Enter task you need done</label>
-                        <input type='text' name='task' id='task' class='form-control' placeholder='Personal Slave'>
-                      </div>
-
-                      <button type='submit' class='btn btn-lg btn-info'>Submit Task</button>
-                    </form>
-
-                    <br><form action='/' method='post'>
-                      <button type='submit' class='btn'>Delete All Tasks</button>
-                    </form>
-
-                  </div> <!-- .col-md-4 -->
-                </div> <!-- .row -->
-              </div> <!-- .container -->
-            </body>
-          </html>
-        ";
-
-    return $output;
+  // 1. Route for home page - The route is where we put the the information together and making sure it's in the correct form (an array, or a string etc)
+    $app->get("/", function() use ($app) {
+        return $app['twig']->render('tasks.html.twig', array('tasks' => Task::getAll()));
     });
 
-    $app->post("/tasks", function() {   // 5. New Route to confirmation page
-        $task = new Tasks($_POST["task"]);   // 6. Instantiate a Task object
+  // 2. Route for sending instantiated new object (new task) to /tasks URL
+    $app->post("/tasks", function() use ($app) {
+        $task = new Task($_POST["task"]);     // Instantiation
         $save = $task->save();
 
-        return "
-          <!DOCTYPE html>
-          <html>
-            <head>
-              <meta charset='utf-8'>
-              <title>Tasks: To Do List</title>
-              <link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css'>
-            </head>
-
-            <body>
-              <div class='container'>
-                <h1>Your new task</h1>
-                <div class='row'>
-                  <div class='col-md-4'>"
-                      . $task->getTask() .
-                      "<br><a href='/'>See All Tasks</a>
-                      </div> <!-- .col-md-4 -->
-                    </div> <!-- .row -->
-                  </div> <!-- .container -->
-                </body>
-              </html>
-              ";
+        return $app['twig']->render('create_task.html.twig', array('newtask' => $task));
     });
 
     $app->post('/', function() {
-        Tasks::deleteAll();
+        Task::deleteAll();
 
         return "
           <!DOCTYPE html>
@@ -127,14 +67,6 @@
                   ";
     });
 
-
-
-
-
     return $app;
-
-
-
-
 
 ?>
--- src/tasks.php
+++ src/tasks.php
@@ -1,5 +1,5 @@
 <?php
-    class Tasks
+    class Task
     {
         private $description;
 
